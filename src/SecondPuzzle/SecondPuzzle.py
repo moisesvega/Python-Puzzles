f94331fb8737268c5b2e0d4ccecb39d8fa492113
Moises Vega <moises@ubuntu.ubuntu-domain>
2011-05-18 00:34:30 -0500

    Add folder for each puzzle and create a new puzzle with a simple file test.

---
+++ src/SecondPuzzle/SecondPuzzle.py
@@ -0,0 +1,15 @@
+'''
+Created on 18 May 2011
+
+@author: Moises Vega
+'''
+
+def meepMeep( fileName ):
+    file = open( fileName, 'r' )
+    #===========================================================================
+    # for line in file:
+    #    print line
+    #===========================================================================
+    print "Meep meep!"
+
+meepMeep( "FileTest.txt" )
